cf8111edc401c57e0da3dd1795869be45bb31ea0
zamorai <35697970+zamorai@users.noreply.github.com>
2020-11-06 14:50:10 -0500

    changed color of input

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -18,7 +18,7 @@ export default function IncomeItem(props) {
   var percentIntensity = percentage > 60 && percentage <= 100 ? "text-green-700" : percentage >= 30 && percentage <= 60 ? 'text-green-500' : 'text-green-300'
 
   return (
-    <div onClick={handleDelete} className="flex justify-between bg-gray-200 py-2">
+    <div onClick={handleDelete} className="flex justify-between bg-blue-100 py-2">
       <div className="flex flex-col items-start ml-8">
         <span className="text-xl">{props.name}</span>
         <span className="text-xs text-gray-500">{props.time}</span>
